00502f5bf5d89e2e50a5f0bc360a48ae6315652b
MUSKAN180918 <148566836+MUSKAN180918@users.noreply.github.com>
2024-04-12 23:53:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop:true,
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+ 
+    const links = document.querySelectorAll("#nav-1 h4 a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                scroll.scrollTo(targetElement);
+            }
+        });
+    });
